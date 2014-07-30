b6060bc928e344990afd3ac15ac71e7525a9c009
AdrienVR <a1vernot@enib.fr>
2014-07-30 11:43:28 +0200

    genial

--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class Main : MonoBehaviour
 {
+	public int nbMap;
 
-void Start()
-{
+	void Start()
+	{
+		Debug.Log ("Demarrage !");
+
 		Kart crash = new Kart(1,2,4);
+		if (nbMap == 2)
+			crash.objet.rigidbody.transform.Translate(50, 0, -40);
 		Kart coco = new Kart(2,2,4);
-}
+	}
 
 }
