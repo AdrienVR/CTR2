9dd80d7e47d432db01b3f2a65f8b292efd8c1fec
AdrienVR <a1vernot@enib.fr>
2014-08-10 12:17:08 +0200

    genial box

---
+++ Assets/WeaponBoxScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponBoxScript : MonoBehaviour {
+
+	public AnimationClip boxClip;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		animation.Play (boxClip.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
