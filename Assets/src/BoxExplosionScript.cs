d172a0345cee3c07e6ae1add8148fcea45286ec8
AdrienVR <a1vernot@enib.fr>
2014-08-12 15:57:40 +0200

    nitro

---
+++ Assets/src/BoxExplosionScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxExplosionScript : MonoBehaviour {
+	
+	public AnimationClip boxClip;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		animation.Play (boxClip.name);
+		GameObject.Find ("nitro").light.color = new Color (.0f, 1.0f, .0f);
+	}
+
+	
+	void OnTriggerExit(Collider other)
+	{
+		GameObject.Find ("nitro").light.color = new Color (.0f, .0f, .0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
