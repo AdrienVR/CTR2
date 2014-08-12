08b4426d59a405b490f30c7793d3e549a4e0d8ab
AdrienVR <a1vernot@enib.fr>
2014-08-12 17:13:54 +0200

    explosions

---
+++ Assets/src/ExplosionScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionScript : MonoBehaviour {
+	
+	public AnimationClip explosionClip;
+	public Color explosionColor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine (Explode());
+	}
+
+	IEnumerator Explode()
+	{
+		if (explosionClip != null)
+			animation.Play (explosionClip.name);
+		gameObject.light.color = explosionColor;
+		yield return new WaitForSeconds (0.3f);
+		Destroy(gameObject);
+	}
+
+	
+	void OnTriggerExit(Collider other)
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
