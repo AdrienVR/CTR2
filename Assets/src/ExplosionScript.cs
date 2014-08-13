c1ff56331886a6cda1f00f266fa04e920da9f4f7
AdrienVR <a1vernot@enib.fr>
2014-08-13 16:44:41 +0200

    correction explosion

--- Assets/src/ExplosionScript.cs
+++ Assets/src/ExplosionScript.cs
@@ -20,7 +20,7 @@ public class ExplosionScript : MonoBehaviour {
 		if (explosionClip != null)
 			animation.Play (explosionClip.name);
 		gameObject.light.color = explosionColor;
-		yield return new WaitForSeconds (0.3f);
+		yield return new WaitForSeconds (0.1f);
 		Destroy(gameObject);
 	}
 
