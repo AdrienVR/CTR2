e886288abbfa0d0b405b03ddf48dfbddd9fddc40
jojolasticot <j1pennec@enib.fr>
2014-10-06 22:53:55 +0200

    Little Pause menu (p)

--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -10,8 +10,10 @@ public class Main : MonoBehaviour
 	public GameObject respawn2;
 	public GameObject respawn3;
 	public GameObject respawn4;
-
 	private List<Transform> listRespawn;
+
+	public bool inPause=false;
+	public float normalTime;
 
 	void Start()
 	{
@@ -20,7 +22,49 @@ public class Main : MonoBehaviour
 		InitializeRespawn ();
 		
 		CreateNPersos(nbPlayer);
+		normalTime = Time.timeScale;
+	}
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			Pause ();
+		}
+	}
 
+	void Pause()
+	{
+		if(!inPause)
+		{
+			Debug.Log("En pause");
+			Time.timeScale=0f;
+			inPause=true;
+			AudioListener.pause = true;
+		}
+		else
+		{
+			Debug.Log("Reprise");
+			Time.timeScale=normalTime;
+			inPause=false;
+			AudioListener.pause = false;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(inPause)
+		{
+			float widthLabel=200;
+			float heightLabel=50;
+			if (GUI.Button(new Rect(Screen.width/2-widthLabel/2, Screen.height/2-heightLabel/2, widthLabel, heightLabel),"QUITTER" ))
+			{
+				Application.Quit();
+			}
+			if (GUI.Button(new Rect(Screen.width/2-widthLabel/2, Screen.height/2-heightLabel/2-heightLabel, widthLabel, heightLabel), "REPRENDRE"))
+			{
+				Pause();
+			}
+		}
 	}
 
 	void InitializeRespawn()
