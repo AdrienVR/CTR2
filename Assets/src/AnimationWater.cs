26be9b5f09b72d6883f61c3d44544b600d21b459
a1vernot@enib.fr <a1vernot@enib.fr>
2015-01-22 17:43:17 +0100

    moving water + texture testing

---
+++ Assets/src/AnimationWater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationWater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		gameObject.transform.position += new Vector3 (0.1f, 0);
+		if (gameObject.transform.position.x > 50f)
+						gameObject.transform.position = new Vector3 ();
+	}
+}
